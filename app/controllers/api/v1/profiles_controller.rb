2a0376545604d80b114f3e3bdae91735fb6d05af
Simone Hoffmann <simohoff@gmx.de>
2020-08-15 17:30:31 +0200

    rendering first card and rendering the cards

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -1,16 +1,51 @@
+require 'open-uri'
+require 'nokogiri'
+
 module Api
   module V1
     class ProfilesController < ApplicationController
       def index
-        @profile
+        userID = '199188713' #die ID muesste am Anfang der App eingegeben werden
+        url = "https://www.dotabuff.com/players/#{userID}"
+
+        html_file = open(url).read
+        html_doc = Nokogiri::HTML(html_file)
+
+        # get the wins
+        html_doc.search('.wins').each do |element|
+          @wins = element.text.strip
+        end
+        # get the losses
+        html_doc.search('.losses').each do |element|
+          @losses = element.text.strip
+        end
+        # get the abandons
+        html_doc.search('.abandons').each do |element|
+          @abandons = element.text.strip
+        end
+
+        # get the win-rate
+        html_doc.search('dd').each do |element|
+          @winRate = element.text.strip
+        end
+
+        # get the latest matches hero
+        @elements = []
+        html_doc.search('.r-body a').each do |element|
+          @elements.push(element.text)
+        end
+
+
+
         render json: {
           status: 'SUCCESS',
           message: 'Loaded',
-          data: @profile
+          data: [@wins,@losses,@abandons, @winRate, @elements]
         }, status: :ok
       end
     end
   end
 end
+
 
 
